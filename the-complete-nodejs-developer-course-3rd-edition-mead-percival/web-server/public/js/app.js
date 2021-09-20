151779d6984505d80e94a40d7682d7dee4fead97
Phil de Guzman <deguzmanphil@gmail.com>
2021-09-20 18:30:43 +0800

    Setup for deployment

--- the-complete-nodejs-developer-course-3rd-edition-mead-percival/web-server/public/js/app.js
+++ the-complete-nodejs-developer-course-3rd-edition-mead-percival/web-server/public/js/app.js
@@ -1,21 +1,3 @@
-// console.log('Test app.js')
-
-// fetch('https://puzzle.mead.io/puzzle').then((response) => {
-//   response.json().then((data) => {
-//     console.log(data)
-//   })
-// })
-
-// const url = 'http://localhost:3000/weather?address=!'
-// fetch(url).then((response) => {
-//   response.json().then((data) => {
-//     if(data.error){
-//       return console.log(data)  
-//     }
-//     console.log(data.location,data.forecast)
-//   })
-// })
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -25,7 +7,7 @@ weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
   const location = search.value
 
-  const url = 'http://localhost:3000/weather?address='+location
+  const url = '/weather?address='+location
   fetch(url).then((response) => {
     response.json().then((data) => {
       if(data.error){
