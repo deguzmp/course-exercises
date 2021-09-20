70d633e2e4f4ff6d40a16b8bb9e7733daa840a77
Phil de Guzman <deguzmanphil@gmail.com>
2021-09-20 17:51:57 +0800

    Added default value for deconstruction.

--- the-complete-nodejs-developer-course-3rd-edition-mead-percival/web-server/src/utils/geocode.js
+++ the-complete-nodejs-developer-course-3rd-edition-mead-percival/web-server/src/utils/geocode.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const geocode = ( address ,callback ) => {
   const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiZGVndXptcCIsImEiOiJja3RsM2w2dHMwbTZ5MnVvNnZqbmE0dWNxIn0.wJR2MvhstgtcqoRcuiB8Vw'
-  request({ url, json:true }, ( error ,{ body } ) => {
+  request({ url, json:true }, ( error ,{ body } = {}) => {
     if ( error ){
       callback('Unable to connect to geocode service.',undefined)
     } else if( body.error ){
